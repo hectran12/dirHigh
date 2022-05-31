9b6fe35835ed1a7cc76c97dcd1ad99aaa63c3a0c
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-05-31 16:47:22 +0700

    demo 0.1
    create , copy , get size

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        dirCsharp.dirHigh dir = new dirCsharp.dirHigh();
+        string tao = dir.createFolder(@"C:\Users\tronghoa\Documents\clone","clone1");
+        if (dir.status)
+        {
+            Console.WriteLine(tao);
+        } else
+        {
+            Console.WriteLine(tao);
+        }
+
+        
+        Console.WriteLine(dir.getSizeFolder(@"C:\Users\tronghoa\Downloads\cmder"));
+
+        dir.DirectoryCopy(@"C:\Users\tronghoa\Downloads\cmder", @"C:\Users\tronghoa\Documents\clone\clone1", true);
+        if (dir.status)
+        {
+            Console.WriteLine("Copy thành công!");
+        }
+    }
+}
